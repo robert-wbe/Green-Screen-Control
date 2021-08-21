1f973fe2c89838f0278fa4dc1770b2edcba60197
Robert Wiebe <robert.wiebe9@icloud.com>
2021-08-21 16:49:09 -0700

    Initial Commit

---
+++ Green Screen Control/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Green Screen Control
+//
+//  Created by Robert Wiebe on 21.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Green Screen Control/Green_Screen_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Green_Screen_ControlApp.swift
+//  Green Screen Control
+//
+//  Created by Robert Wiebe on 21.08.21.
+//
+
+import SwiftUI
+
+@main
+struct Green_Screen_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
